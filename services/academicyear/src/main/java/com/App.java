2c17b86141e74035e49fd0071c65584a596d2c8b
Erwan <erwan.lebras27@gmail.com>
2025-03-05 23:30:45 +0100

    fix : unused attribute + port
    Delete unused attributes + change the port from 8080 to 8081

--- services/academicyear/src/main/java/com/App.java
+++ services/academicyear/src/main/java/com/App.java
@@ -7,10 +7,6 @@ import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class App {
-	@Bean
-	public RestTemplate restTemplate() {
-		return new RestTemplate();
-	}
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
 	}
